b7ed1c565bcda953a7db2e96ce72066277337cfe
BubbLeDg <132152810+BubbLeDg@users.noreply.github.com>
2023-05-06 17:47:31 +0700

    Update test_items.py
    fixing css_selector

--- test_items.py
+++ test_items.py
@@ -6,5 +6,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_search_button_shopping_cart(browser):
     browser.get(link)
     time.sleep(3)
-    button = browser.find_element_by_css_selector(".add-to-basket .btn")
+    button = browser.find_element(By.CSS_SELECTOR, ".add-to-basket .btn")
     assert button, "Should be button with the addition of the product to cart"
